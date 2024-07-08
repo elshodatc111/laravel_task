8690e37cd45cac03c90656badb538bda4f18f666
elshodatc111 <elshodatc1116@gmail.com>
2024-07-08 23:25:31 +0500

    Clent creat post

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -1,10 +1,39 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Application;
 use Illuminate\Http\Request;
+
 
 class ApplicationController extends Controller
 {
-    //
+    public function store(Request $request){
+        if($request->hasFile('file')){
+            $name = $request->file('file')->getClientOriginalName();
+            $path = $request->file('file')->storeAs(
+                'files',
+                $name,
+                'public'
+            );
+        }
+        // Storgae papkasiga joylangan fayllarni public papkasiga joylashtiriladi
+        // php artisan storage:link
+        
+        
+        $request->validate([ 
+            'subject'=>'required|max:255',
+            'message'=>'required',
+            'file' => 'file|mimes:jpg,png,pdf'
+        ]); // Fake Filer
+
+        $Application = Application::create([
+            'user_id' => auth()->user()->id,
+            'subject' => $request->subject,
+            'message' => $request->message,
+            'file_url' => $path??null
+        ]);
+
+        return redirect()->back();
+
+    }
 }
